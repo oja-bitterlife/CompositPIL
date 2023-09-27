47a1913dab4e3708558ed055f26eb6b8abcb1352
oja-bitterlife <bitter.life.club@gmail.com>
2023-09-27 16:27:34 +0900

    ポートも変更できるように

--- Addon/UI_SendPIL.py
+++ Addon/UI_SendPIL.py
@@ -4,7 +4,7 @@ from urllib.parse import urlencode
 
 # DEFINE
 SERVER_PORT = 8080
-CANNY_SERVER_URL = "http://localhost:{:d}/canny".format(SERVER_PORT)
+CANNY_SERVER_URL = "http://localhost:{:d}/canny"
 
 
 # Data Structure
@@ -64,7 +64,7 @@ class COMPOSIT_PIL_OT_run_all(bpy.types.Operator):
     def execute(self, context):
 
         for i in range(len(context.scene.canny_data)):
-            response = run(context, CANNY_SERVER_URL, i)
+            response = run(context, CANNY_SERVER_URL.format(context.scene.canny_server_port), i)
 
         # 自動更新
         if context.scene.canny_after_reload:
@@ -81,7 +81,7 @@ class COMPOSIT_PIL_OT_run(bpy.types.Operator):
     id: bpy.props.IntProperty()
 
     def execute(self, context):
-        response = run(context, CANNY_SERVER_URL, self.id)
+        response = run(context, CANNY_SERVER_URL.format(context.scene.canny_server_port), self.id)
 
         # 自動更新
         if context.scene.canny_after_reload:
@@ -144,6 +144,14 @@ def draw(self, context):
         # 実行ボタン
         img_box.operator("composit_pil.run").id = i
 
+    # サーバポート指定
+    row = self.layout.box().row().split(align=True, factor=0.2)
+    row.prop(context.scene, "canny_server_port_lock", text="Lock")  # 実行後リロード
+    row_child = row.row()
+    if context.scene.canny_server_port_lock:
+        row_child.enabled = False
+    row_child.prop(context.scene, "canny_server_port", text="Server Port")
+
     # 全部実行ボタン
     self.layout.operator("composit_pil.run_all")
 
@@ -155,6 +163,9 @@ def register():
     bpy.types.Scene.canny_data = bpy.props.CollectionProperty(type=CANNY_DATA)
     bpy.types.Scene.canny_after_reload = bpy.props.BoolProperty(name="after reload", default=True)
     bpy.types.Scene.canny_scale_type = bpy.props.EnumProperty(name = "image type", items=SCALE_TYPES, default="x2c-down")
+
+    bpy.types.Scene.canny_server_port_lock = bpy.props.BoolProperty(name="server port lock", default=True)
+    bpy.types.Scene.canny_server_port = bpy.props.IntProperty(name="server port", default=8080, min=0, max=65535)
 
 def unregister():
     pass
