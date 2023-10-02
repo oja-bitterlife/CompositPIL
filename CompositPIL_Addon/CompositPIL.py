d00e6662f70d3419e6ab465754892fae7dca6e08
oja-bitterlife <bitter.life.club@gmail.com>
2023-10-02 18:22:21 +0900

    カテゴリ名そとだし

--- CompositPIL_Addon/CompositPIL.py
+++ CompositPIL_Addon/CompositPIL.py
@@ -2,6 +2,9 @@ import bpy
 
 from . import UI_SendPIL
 
+# UIカテゴリ名
+COMPIL_CATEGORY = "CompositPIL"
+
 # Main UI
 # ===========================================================================================
 # Compositing Tools Panel
@@ -9,7 +12,7 @@ class COMPOSIT_PIL_PT_ui(bpy.types.Panel):
     bl_label = "ConvertCanny"
     bl_space_type = "NODE_EDITOR"
     bl_region_type = "UI"
-    bl_category = "CompositPIL"
+    bl_category = COMPIL_CATEGORY
     bl_idname = "COMPOSIT_PIL_PT_UI"
 #    bl_options = {'DEFAULT_CLOSED'}
 
