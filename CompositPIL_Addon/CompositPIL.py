614ba26f3e68a8fb9efb6478bac3fa10eae7ac21
oja-bitterlife <bitter.life.club@gmail.com>
2023-10-02 21:49:54 +0900

    UI修正

--- CompositPIL_Addon/CompositPIL.py
+++ CompositPIL_Addon/CompositPIL.py
@@ -8,23 +8,22 @@ COMPIL_CATEGORY = "CompositPIL"
 # Main UI
 # ===========================================================================================
 # Compositing Tools Panel
-class COMPOSIT_PIL_PT_ui(bpy.types.Panel):
+class COMPOSIT_PIL_PT_node_ui(bpy.types.Panel):
     bl_label = "ConvertCanny"
     bl_space_type = "NODE_EDITOR"
     bl_region_type = "UI"
     bl_category = COMPIL_CATEGORY
-    bl_idname = "COMPOSIT_PIL_PT_UI"
-#    bl_options = {'DEFAULT_CLOSED'}
+    bl_idname = "COMPOSIT_PIL_PT_NODE_UI"
 
     def draw(self, context):
         UI_SendPIL.draw(self, context)
 
-class COMPOSIT_PIL_PT_ui(bpy.types.Panel):
+class COMPOSIT_PIL_PT_view_ui(bpy.types.Panel):
     bl_label = "ConvertCanny"
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     bl_category = COMPIL_CATEGORY
-    bl_idname = "COMPOSIT_PIL_PT_UI"
+    bl_idname = "COMPOSIT_PIL_PT_VIEW_UI"
 
     def draw(self, context):
         UI_AppendNodes.draw(self, context)
